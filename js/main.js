963d9ed4fbf20eac2402c68f61fb3576c1b4363e
michelthomas2 <michelthomas@ufl.edu>
2020-11-13 20:04:47 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let bigImage = document.querySelector('#waterfall');
+
+// click event
+
+bigImage.onclick = () => {
+   if ( bigImage.getAttribute('src') ===
+   'images/waterfallbw.jpg' )
+   {
+        bigImage.setAttribute('src',
+        'images/waterfall.jpg');
+   } else {
+        bigImage.setAttribute('src',
+        'images/waterfallbw.jpg');
+   }
+}
+
+let buns = document.querySelector('#bunny');
+
+buns.onmouseover = () => {
+  if (buns.getAttribute('src') === 'images/bunny.jpg')
+  {
+    buns.setAttribute('src', 'images/topo.jpg');
+  } else if (buns.getAttribute('src') === 'images/topo.jpg') {
+    buns.setAttribute('src',
+    'images/dog.jpg');
+  } else {
+    buns.setAttribute('src', 'images/bunny.jpg');
+  }
+}
+
+// click event
